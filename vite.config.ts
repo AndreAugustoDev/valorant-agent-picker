28c9562c7abc047354e4fa498c6c9cb14d417d8b
André Augusto <25272842+AndreAugustoDev@users.noreply.github.com>
2026-08-24 20:12:15 -0300

    ✨ feat: add GitHub Actions workflow for deployment to GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
-import { svelte } from "@sveltejs/vite-plugin-svelte";
-import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 import { intlayer } from "vite-intlayer";
+import { svelte } from "@sveltejs/vite-plugin-svelte";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "./",
   plugins: [svelte(), intlayer(), tailwindcss()],
 });
